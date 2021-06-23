7f134016e0e1cf1ff3b8f40547b94e98f733d238
PrajwalParvati <parvatiprajwal@gmail.com>
2021-06-23 10:57:56 -0700

    updating our app with a header,and Link to home component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 import HomePage from './component/pages/homepage/homepage.component'; 
 import ShopPage from './component/shop/shop.component';
 import {Switch, Route } from 'react-router-dom';
-import { div } from 'prelude-ls';
-
+import Header from './component/header/header.component';
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/shop' component={ShopPage}/>
